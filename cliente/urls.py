2af95541a1850a907f05d4fd59c4937f86263d1c
Talita Rossari <rossaritalita@gmail.com>
2018-08-06 21:25:23 -0300

    Criando a Url e view do editar cliente. Tamnbém mudando os nomes do arquivo de template para ficar em portugues

--- cliente/urls.py
+++ cliente/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'cliente'
 urlpatterns = [
     path('lista/', views.ClienteListView.as_view(), name='lista'),
+    path('<int:pk>/editar', views.ClienteUpdateView.as_view(), name='editar'),
 ]
--- cliente/views.py
+++ cliente/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from cliente.models import Cliente
+from django.views.generic.edit import UpdateView
 from django.views import generic
 
 
@@ -9,5 +11,13 @@ from django.views import generic
 # Provavelmente tem um jeito bonito de se fazer.
 class ClienteListView(generic.ListView):
     model = Cliente
-    template_name = 'cliente_list.html'
+    template_name = 'cliente_lista.html'
     context_object_name = 'clientes'
+
+
+class ClienteUpdateView(UpdateView):
+    model = Cliente
+    template_name = 'cliente_editar.html'
+    fields = '__all__'
+    context_object_name = 'cliente'
+    success_url = reverse_lazy('cliente:lista')
